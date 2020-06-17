c7331d96472e8266aea2c946a10c395100cc1fd1
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-06-17 14:00:52 +0300

    try

--- driver/driver.js
+++ driver/driver.js
@@ -1,62 +1,20 @@
 'use strict';
 
-const net = require('net');
-const client = new net.Socket();
+const io = require('socket.io-client');
+const caps = io.connect('http://localhost:3000/caps');
 
+caps.emit('join', 'driver');
 
-const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.PORT || 3000;
+caps.on('msg', data => { 
+  setTimeout(() => {
+    console.log(`picked up ${data.payload.orderID}`);
+    let event = { event: 'in-transit', time: new Date(), payload: data.payload };
+    caps.emit('in-transit', event);
+  }, 1000);
 
-client.connect(PORT, HOST, ()=> {console.log('driver got connected');});
-let end ;
-let end1;
-client.on('data', function(data) {
- 
-  let event = JSON.parse(data);
-  
-  if (event.event === 'pic-up'){
-    end = setTimeout(function(){
-      console.log(`picked up ${event.payload.orderId}`);
-      let nowEvent = JSON.stringify({ event: 'in-transit',time:new Date(), payload: event.payload });
-      client.write(nowEvent);
-    }, 1000);
-            
-    end1 =setTimeout(function(){
-      console.log(`delivered ${event.payload.orderId}`);
-      let nowEvent1 = JSON.stringify({ event: 'delivered',time:new Date(), payload: event.payload });
-      client.write(nowEvent1);
-       
-    }, 3000);
-  }
-     
- 
+  setTimeout(() => {
+    console.log(`delivered up ${data.payload.orderID}`);
+    let event = { event: 'delivered', time: new Date(), payload: data.payload };
+    caps.emit('delivered', event);
+  }, 3000);
 });
-
-client.on('close', function() {
-  console.log('driver Connection got closed');
-  clearTimeout(end);
-  clearTimeout(end1);
-});
-
-
-
-
-// 'use strict';
-// const events = require('./events');
-// function timing(paylod){
-  
-//   setTimeout(function(){
-
-//     console.log(`DRIVER: picked up ${paylod.orderId}`);
-//     events.emit('in-transit',paylod);
-//   }, 1000);
-    
-//   setTimeout(function(){
-//     console.log('delivered');
-//     events.emit('delivered',paylod);
-//     console.log('WELCOME!!!!');
-//   }, 3000);
-    
-// }
-
-// module.exports = timing;
--- vender/vendor.js
+++ vender/vendor.js
@@ -1,70 +1,23 @@
+
 'use strict';
-require('dotenv').config();
-var faker = require('faker');
-const net = require('net');
 
-const client = new net.Socket();
+const faker = require('faker');
+const io = require('socket.io-client');
+const caps = io.connect('http://localhost:3000/caps');
 
-const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.PORT || 3000;
+caps.emit('join', 'vendor');
 
-client.connect(PORT, HOST, ()=> {console.log('#####vendor got connected####');});
-
-
-
-client.on('data', function(data){ 
-  // when the server sends me back any data
-  let eventObj = JSON.parse(data);
-  if (eventObj.event === 'delivered') {
-    console.log('Thank you!!');
-  }
+caps.on('msg',data =>{
+  console.log('Thank you',data.payload.orderID);
 });
 
-
-function sendMessage(user) {
-  let event = JSON.stringify({ event: 'pic-up',time:new Date(), payload: user });
-  client.write(event);
-}
-  
-  
-
-
-let end = setInterval (function getData() {
-  let user = {
-    randomName: faker.name.findName(),
-    orderId:faker.random.uuid(),
-    addres:faker.address.city(),
-    storeName:process.env.STORENAME || 'mod',
+setInterval(() => {
+  let data = {
+    storeName: process.env.STORENAME || 'FOREVERYTHING STORE',
+    customerName: faker.name.findName(),
+    orderID: faker.random.uuid(),
+    address: faker.address.streetAddress(),
   };
-    
-  sendMessage(user);
+  let event = { event: 'pickup', time: new Date(), payload: data };
+  caps.emit('pickup', event);
 }, 5000);
-  
- 
-client.on('close', function() {
-  console.log('driver Connection got closed');
-  clearInterval(end);
-});
-
-
-
-
-// 'use strict';
-// require('dotenv').config();
-// var faker = require('faker');
-// const events = require('./events');
-
-// setInterval(function(){
-
-//   let user = {
-//     randomName: faker.name.findName(),
-//     orderId:faker.random.uuid(),
-//     addres:faker.address.city(),
-//     storeName:process.env.STORENAME,
-//   };
-
-//   events.emit('pickup', user );
-// }, 5000);
-
-
-    
